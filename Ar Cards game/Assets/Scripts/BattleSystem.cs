ac82ec284f00244dfa7c85d4d4e80f74e62e7063
Francioo87NABA <francesco_palma@studenti.naba.it>
2020-11-01 11:55:37 +0100

    Unit script
    aggiunto l unit script e il battle system, due totem di prova

---
+++ Ar Cards game/Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState { START, YTOTEM, ZTOTEM, YMAGIA, ZMAGIA }
+
+public class BattleSystem : MonoBehaviour
+{
+    public BattleState State;
+
+    public Image startInstructions;
+    public Button fatto;
+    
+    void Start()
+    {
+        State = BattleState.START;
+        StartCoroutine(SetUpBattle());
+    }
+
+    IEnumerator SetUpBattle()
+    {
+        startInstructions.gameObject.SetActive(true);
+        
+        yield return new WaitForSeconds(4f);
+        
+        fatto.gameObject.SetActive(true);
+        
+    }
+    
+}
---
+++ Ar Cards game/Assets/Scripts/Unit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public string name;
+
+    public int maxHealth;
+    public int currentHealth;
+    public int damage;
+}
